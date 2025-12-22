efa32f37f9b899b76b7a9ad505f1de3949b62ee1
Tom Wright <thomasjameswright@hotmail.co.uk>
2025-12-22 13:16:30 +0000

    red balloon and blue balloon death working

--- scenes/blue_balloon.gd
+++ scenes/blue_balloon.gd
@@ -1,7 +1,11 @@
 extends PathFollow2D
 
-@export var speed := 0.02
+@export var speed := 0.1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	progress_ratio += speed * delta
+	
+	if progress_ratio >= 0.2:
+		queue_free()
+	
--- scenes/end_point.gd
+++ scenes/end_point.gd
@@ -2,5 +2,4 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("entered")
-	print(body)
+	body.queue_free()
