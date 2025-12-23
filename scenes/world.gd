0c5f29f7d476344ad1bf3f10a53bcc59980fd395
Joe Dickinson <josephd850@gmail.com>
2025-12-23 10:54:20 +0000

    Added green balloon, updated speed to make green balloon quicker than blue

--- scenes/world.gd
+++ scenes/world.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 # Load balloon scene
 var blue_balloon_scene: PackedScene = load("res://scenes/blue_balloon.tscn")
+var green_balloon_scene: PackedScene = load("res://scenes/green_balloon.tscn")
 
 func _on_balloon_timer_timeout() -> void:
-	var blue_balloon = blue_balloon_scene.instantiate()
+	var rng := RandomNumberGenerator.new()
+	var random_number = rng.randf_range(-1.0, 1.0)
 	
-	$Path2D.add_child(blue_balloon)
+	if random_number > 0:
+		$Path2D.add_child(blue_balloon_scene.instantiate())
+	else:
+		$Path2D.add_child(green_balloon_scene.instantiate())
