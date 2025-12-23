315cb3d8b70d773750efe1caba07f4474a70507b
Joe Dickinson <josephd850@gmail.com>
2025-12-23 15:49:46 +0000

    Remove balloon looping of path and update balloon end of path killing to simplify, update balloon kill logging

--- scenes/blue_balloon.gd
+++ scenes/blue_balloon.gd
@@ -1,22 +1,15 @@
 extends PathFollow2D
 
 @export var SPEED := 0.05
-@export var LIFETIME := 1
-# When, on a scale of 0-1, entity will be killed
 
 var green_balloon_scene: PackedScene = load("res://scenes/green_balloon.tscn")
-var new_progress
 
 func _process(delta: float) -> void:
-	new_progress = progress_ratio + SPEED * delta
-	# Calculates theoretical new progress
-	
-	if new_progress >= LIFETIME:
-		# If new progress is larger than defined LIFETIME, kill entity
-		print("KILLED ", name)
+	progress_ratio += SPEED * delta
+
+	if progress_ratio >= 1.0:
+		print(name, " reached the exit")
 		queue_free()
-	else:
-		progress_ratio = new_progress
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	var new_balloon = green_balloon_scene.instantiate()
@@ -30,6 +23,7 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	# Defers adding new balloon and setting of new balloons progress
 	# Necessary to avoid collisions changing while inside area_entered
 	
+	print(name, " was popped")
 	area.queue_free()
 	queue_free()
 	# Kills projectile and current balloon
