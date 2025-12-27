f1ddc66e446b1e7807918d53dbf7a9668e026ab1
Joe Dickinson <josephd850@gmail.com>
2025-12-27 18:28:16 +0000

    Addd UI script and game_scene logic framework for building towers

---
+++ scenes/game_scene.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+var map_node
+
+var build_mode = false
+var build_valid = false
+var build_location
+var build_type
+
+func _ready() -> void:
+	# For each build button, binds the button to the initiate_build_mode function
+	for i in get_tree().get_nodes_in_group("BuildButtons"):
+		# Adds the tower name as a parameter passed to the binded function
+		i.pressed.connect(initiate_build_mode.bind(i.get_name()))
+
+func initiate_build_mode(tower_type):
+	build_type = tower_type
+	build_mode = true
+	$UI.set_tower_preview(build_type, get_global_mouse_position())
+	pass
+	
+func _process(_delta: float) -> void:
+	if build_mode:
+		update_tower_preview()
+
+func update_tower_preview():
+	var mouse_position = get_global_mouse_position()
+	
+	#if tower collision not overlapping any other tower collisions or map collisions
+		# $UI.update_tower_preview(good)
+		# build_valid = true
+		# build_location = mouse_position
+	#else:
+		# $UI.update_tower_preview(bad)
+		# build_valid = false
+	
+func cancel_build_mode():
+	pass
---
+++ scenes/ui.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+func set_tower_preview(tower_type, mouse_position):
+	var drag_tower = load("res://scenes/towers/lamprey_tower.tscn").instantiate()
+	drag_tower.set_name("DragTower")
+	drag_tower.modulate = Color("ff5a76")
+	
+	var control = Control.new()
+	control.add_child(drag_tower, true)
+	control.set_position(mouse_position)
+	control.set_name("TowerPreview")
+	add_child(control, true)
+	move_child(control, 0)
