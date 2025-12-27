de03bae56ffe659c71580ffb005c2c2b51cef949
Tom Wright <thomasjameswright@hotmail.co.uk>
2025-12-27 11:14:46 +0000

    added scene handler

---
+++ scenes/scene_handler.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func _ready():
+	get_node("MainMenu/Margin/VBoxContainer/Play").connect("pressed", on_play_pressed)
+	get_node("MainMenu/Margin/VBoxContainer/Quit").connect("pressed", on_quit_pressed)
+
+func on_play_pressed():
+	get_node("MainMenu").queue_free()
+	var game_scene = load("res://scenes/game_scene.tscn").instantiate()
+	add_child(game_scene)
+
+func on_quit_pressed():
+	get_tree().quit()
