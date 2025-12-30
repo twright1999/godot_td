603e9751571de3cabc12a16e99e2a06c4f52785e
Joe Dickinson <josephd850@gmail.com>
2025-12-30 17:53:39 +0000

    Correct on_area_exited to erase parent instead of area (area will not be in array)

--- scenes/towers/targeting_component.gd
+++ scenes/towers/targeting_component.gd
@@ -6,7 +6,7 @@ func _on_area_entered(area: Area2D) -> void:
 	targets_in_range.append(area.get_parent())
 	
 func _on_area_exited(area: Area2D) -> void:
-	targets_in_range.erase(area)
+	targets_in_range.erase(area.get_parent())
 	
 func get_first_target() -> Node2D:
 	var highest_progress = 0
@@ -15,5 +15,5 @@ func get_first_target() -> Node2D:
 		if balloon.progress_ratio > highest_progress:
 			highest_progress = balloon.progress_ratio
 			target_balloon = balloon
-			
+	
 	return target_balloon
