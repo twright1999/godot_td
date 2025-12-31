ccf4b8f445ae1c68d33a487d2e17eb370580c7c1
Tom Wright <thomasjameswright@hotmail.co.uk>
2025-12-31 14:01:46 +0000

    updated timeout

--- scenes/projectiles/projectile_component.gd
+++ scenes/projectiles/projectile_component.gd
@@ -1,20 +1,20 @@
 extends Area2D
 
+@export var lifetime := 0.5
 @export var projectile_damage : int
 @export var speed : int
 @export var damage_type : DataTypes.Resistance
 
-@export var lifetime : float
-@onready var lifetime_timer = $Timer
-
 signal lifetime_expired
 
-func ready():
+@onready var lifetime_timer = $Timer
+
+func _ready():
 	lifetime_timer.wait_time = lifetime
+	lifetime_timer.start()
+	
+func _on_timer_timeout() -> void:
+	lifetime_expired.emit()
 
 func move(projectile_rotation, delta) -> Vector2:
 	return Vector2(1, 0).rotated(projectile_rotation) * speed * delta
-
-func _on_timer_timeout() -> void:
-	print("timer expired")
-	lifetime_expired.emit()
