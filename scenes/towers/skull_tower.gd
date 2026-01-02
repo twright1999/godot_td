890f22c7573f1f750753bf385c9b75827584d0d7
Tom Wright <thomasjameswright@hotmail.co.uk>
2026-01-02 11:33:10 +0000

    init of skull tower

---
+++ scenes/towers/skull_tower.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@export var projectile_container: Node2D		## World scene container for projectiles
+
+@onready var targeting = $TargetingComponent
+@onready var base = $BaseComponent
+
+@export var proj_count := 8
+
+var built = true
+var target = null
+
+func _physics_process(_delta: float) -> void:
+	if built:
+		target = targeting.get_first_target()
+
+func _on_turret_component_fire_projectile(projectile: Node) -> void:
+	if built and target:
+		spawn_fireballs(proj_count, projectile)
+		
+func spawn_fireballs(count: int, proj: Node) -> void:
+	for angle in range(0, 360, 360.0/count):
+		var new_proj = proj.duplicate()
+		new_proj.rotation = deg_to_rad(angle)
+		projectile_container.add_child(new_proj)
