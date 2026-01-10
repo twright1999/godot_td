c05dc96b00149c3e6f2295114fdfc741629dc0fa
Joe Dickinson <josephd850@gmail.com>
2026-01-10 15:04:24 +0000

    Added debug menu to world scene for testing features

---
+++ scenes/game_logic/debug_menu.gd
@@ -0,0 +1,15 @@
+extends HBoxContainer
+
+func _on_debug_damage_pressed() -> void:
+	MoneyHealthManager.take_health(50)
+
+func _on_debug_add_money_pressed() -> void:
+	MoneyHealthManager.add_money(200)
+
+func _on_debug_speed_up_pressed() -> void:
+	if Engine.time_scale == 1:
+		Engine.time_scale = 10
+		Engine.physics_ticks_per_second = 600
+	else:
+		Engine.time_scale = 1
+		Engine.physics_ticks_per_second = 60
