9c887fc7b2f44e5b3883f5b6191e3394591cb905
Joe Dickinson <josephd850@gmail.com>
2026-01-10 15:08:13 +0000

    Slight tweak to debug menu speedup button for clarity

--- scenes/game_logic/debug_menu.gd
+++ scenes/game_logic/debug_menu.gd
@@ -7,9 +7,9 @@ func _on_debug_add_money_pressed() -> void:
 	MoneyHealthManager.add_money(200)
 
 func _on_debug_speed_up_pressed() -> void:
-	if Engine.time_scale == 1:
-		Engine.time_scale = 10
-		Engine.physics_ticks_per_second = 600
-	else:
+	if Engine.time_scale == 10:
 		Engine.time_scale = 1
 		Engine.physics_ticks_per_second = 60
+	else:
+		Engine.time_scale = 10
+		Engine.physics_ticks_per_second = 600
