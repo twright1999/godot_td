2ae26009a2beae182ecc87c62851051706ecb012
Joe Dickinson <josephd850@gmail.com>
2026-01-11 14:18:45 +0000

    Restructure of code to split into build and focus sections. Made tower automatically focus when built

--- scenes/game_logic/game_scene.gd
+++ scenes/game_logic/game_scene.gd
@@ -19,6 +19,22 @@ func _ready() -> void:
 		# Adds the tower name as a parameter passed to the binded function
 		i.pressed.connect(initiate_build_mode.bind(i.Tower))
 
+func _process(_delta: float) -> void:
+	if build_mode:
+		update_tower_preview()
+		CursorHandler.set_state(CursorHandler.CursorState.GRAB)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_released("ui_cancel") and build_mode == true:
+		cancel_build_mode()
+	if event.is_action_released("ui_accept") and build_mode == true:
+		verify_and_build()
+	if event.is_action_pressed("ui_accept") and focusing_on_tower:
+		cancel_focus_mode()
+
+##
+## Build Mode Functionality
+##
 func initiate_build_mode(tower_type):
 	# Initiates build mode for tower_type binded to button in _ready()
 	if build_mode:
@@ -32,6 +48,53 @@ func initiate_build_mode(tower_type):
 	# Begin previewing tower_type to build
 	$UI.set_tower_preview(build_type, get_global_mouse_position())
 
+func update_tower_preview():
+	var mouse_position = get_global_mouse_position()
+	var drag_tower = get_node("UI/TowerPreview/DragTower")
+	
+	# Checks if tower is colliding with pre-existing towers or designated
+	#	path exclusion area
+	if drag_tower.base and drag_tower.base.is_building_colliding():
+		# Set tower preview to red shade and disallow building
+		$UI.update_tower_preview(mouse_position, "ff5a76")
+		build_valid = false
+	else:
+		# Set tower preview to green shade and allow building
+		$UI.update_tower_preview(mouse_position, "3ff05f")
+		build_valid = true
+		build_location = mouse_position
+
+func verify_and_build():
+	# If tower can be built, create tower at position and set built to true
+	if build_valid:
+		var new_tower = build_type.instantiate()
+		
+		if MoneyHealthManager.take_money(new_tower.get_node("BaseComponent").tower_cost):
+			new_tower.projectile_container = projectile_container
+			new_tower.position = build_location
+			
+			if new_tower.get_node("GrabPoint"):
+				new_tower.position -= new_tower.get_node("GrabPoint").position
+				
+			new_tower.get_node("BaseComponent").built = true
+			new_tower.get_node("BaseComponent").connect("tower_clicked", _on_tower_clicked)
+			$World.get_node("Towers").add_child(new_tower)
+			initiate_focus(new_tower)
+		else:
+			new_tower.queue_free()
+			cancel_build_mode()
+
+func cancel_build_mode():
+	build_mode = false
+	build_valid = false
+	
+	CursorHandler.set_state(CursorHandler.CursorState.DEFAULT)
+	# Remove tower preview
+	get_node("UI/TowerPreview").free()
+
+##
+## Focus Mode Functionality
+##
 func initiate_focus(tower):
 	if build_mode:
 		cancel_build_mode()
@@ -49,61 +112,5 @@ func focus_on_tower(tower):
 	focusing_on_tower = true
 	currently_focused_tower = tower
 
-func _process(_delta: float) -> void:
-	if build_mode:
-		update_tower_preview()
-		CursorHandler.set_state(CursorHandler.CursorState.GRAB)
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_released("ui_cancel") and build_mode == true:
-		cancel_build_mode()
-	if event.is_action_released("ui_accept") and build_mode == true:
-		verify_and_build()
-		cancel_build_mode()
-	if event.is_action_pressed("ui_accept") and focusing_on_tower:
-		cancel_focus_mode()
-
-func verify_and_build():
-	# If tower can be built, create tower at position and set built to true
-	if build_valid:
-		var new_tower = build_type.instantiate()
-		
-		if MoneyHealthManager.take_money(new_tower.get_node("BaseComponent").tower_cost):
-			new_tower.projectile_container = projectile_container
-			new_tower.position = build_location
-			
-			if new_tower.get_node("GrabPoint"):
-				new_tower.position -= new_tower.get_node("GrabPoint").position
-				
-			new_tower.get_node("BaseComponent").built = true
-			new_tower.get_node("BaseComponent").connect("tower_clicked", _on_tower_clicked)
-			$World.get_node("Towers").add_child(new_tower)
-		else:
-			new_tower.queue_free()
-
 func _on_tower_clicked(tower):
 	initiate_focus(tower)
-
-func update_tower_preview():
-	var mouse_position = get_global_mouse_position()
-	var drag_tower = get_node("UI/TowerPreview/DragTower")
-	
-	# Checks if tower is colliding with pre-existing towers or designated
-	#	path exclusion area
-	if drag_tower.base and drag_tower.base.is_building_colliding():
-		# Set tower preview to red shade and disallow building
-		$UI.update_tower_preview(mouse_position, "ff5a76")
-		build_valid = false
-	else:
-		# Set tower preview to green shade and allow building
-		$UI.update_tower_preview(mouse_position, "3ff05f")
-		build_valid = true
-		build_location = mouse_position
-		
-func cancel_build_mode():
-	build_mode = false
-	build_valid = false
-	
-	CursorHandler.set_state(CursorHandler.CursorState.DEFAULT)
-	# Remove tower preview
-	get_node("UI/TowerPreview").free()
