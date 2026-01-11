9438e8ddb56da31c3006c57c79f06c6f185c11cf
Joe Dickinson <josephd850@gmail.com>
2026-01-11 21:20:41 +0000

    Fix wave end breaking by emitting no_children constantly when path has no more balloons

--- scenes/game_logic/balloon_path.gd
+++ scenes/game_logic/balloon_path.gd
@@ -4,12 +4,13 @@ var children_balloons = 0
 
 signal no_children
 
+func _physics_process(_delta: float) -> void:
+	if children_balloons == 0:
+		no_children.emit()
+
 func _on_child_exiting_tree(node: Node) -> void:
 	if node is PathFollow2D:
 		children_balloons -= 1
-
-	if children_balloons == 0:
-		no_children.emit()
 
 func _on_child_entered_tree(node: Node) -> void:
 	if node is PathFollow2D:
